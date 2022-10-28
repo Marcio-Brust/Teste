4c2de9d37e7d27b230bfa542c8c66fb7d48dbd21
Marcio-Brust <brustmw21@gmail.com>
2022-10-28 00:41:10 -0300

    correcao js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const body = document.querySelector("body");
 const btn = document.querySelector(".btn");
 const eventos = ["click", "touchstart"];
 
-const handleClick = ({ type }) => {
-  if (type === "touchstart") event.preventDefault();
+const handleClick = (event) => {
+  if (event.type === "touchstart") event.preventDefault();
   body.classList.toggle("active");
 
   const active = body.classList.contains("active");
